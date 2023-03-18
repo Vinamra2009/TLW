437256416e4eee352c37d72d24484af0f6833da2
Vinamra2009 <vsulgante2000@gmail.com>
2023-03-18 20:44:04 +0530

    I

--- profiles_api/admin.py
+++ profiles_api/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from profiles_api import models
 
 # Register your models here.
+#This line registers UserProfile to be accesed through the admin side
+admin.site.register(models.UserProfile)
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+#Install the api view and the response object from rest_framework
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+
 
 # Create your views here.
+class HelloApiView(APIView):
+    #Demo to accept get request
+    #So whenever a get request is made to this APIview it will check for the get function in views
+
+    def get(self, request, format=None):
+        an_apiview = [
+            'USES HTTP methods',
+            'Similar to Django view',
+            'Gives most control',
+        ]
+
+        return Response({'message':'Hello','an_apiview': an_apiview})
